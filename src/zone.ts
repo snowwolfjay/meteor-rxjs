c07c81ceef36734ee7e59de13d823d57cc5da192
yuhj <by4474@bangyan.com.cn>
2022-06-10 09:59:19 +0800

    update rxjs dep

--- src/zone.ts
+++ src/zone.ts
@@ -1,40 +1,27 @@
-import { Observable ,  Operator ,  Subscriber } from 'rxjs';
+import { Observable } from "rxjs";
 
-import { getZone } from './utils';
+import { getZone } from "./utils";
 
-export const zoneOperator = <T>(zone?: Zone) => (source: Observable<T>) => source.lift(new ZoneOperator(zone || getZone()));
-
-
-class ZoneOperator<T> implements Operator<T, T> {
-  constructor(private zone: Zone) {
-  }
-
-  call(subscriber: Subscriber<T>, source: any) {
-    return source._subscribe(new ZoneSubscriber(subscriber, this.zone));
-  }
-}
-
-class ZoneSubscriber<T> extends Subscriber<T> {
-  constructor(destination: Subscriber<T>,
-              private zone: Zone) {
-    super(destination);
-  }
-
-  protected _next(value: T) {
-    this.zone.run(() => {
-      this.destination.next(value);
+export const zoneOperator =
+  <T>(zone?: Zone) =>
+  (source: Observable<T>) =>
+    new Observable((suber) => {
+      zone = zone || getZone();
+      return source.subscribe({
+        next(value) {
+          zone.run(() => {
+            suber.next(value);
+          });
+        },
+        complete() {
+          zone.run(() => {
+            suber.complete();
+          });
+        },
+        error(err) {
+          zone.run(() => {
+            suber.error(err);
+          });
+        },
+      });
     });
-  }
-
-  protected _complete() {
-    this.zone.run(() => {
-      this.destination.complete();
-    });
-  }
-
-  protected _error(err?: any) {
-    this.zone.run(() => {
-      this.destination.error(err);
-    });
-  }
-}
